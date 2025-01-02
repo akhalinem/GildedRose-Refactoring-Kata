738139f9c8973a2bf842c3b83bcbd0a816e23e02
Abror Khalilov <akhalinem@gmail.com>
2025-01-02 14:09:12 +0000

    Improve code

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -13,21 +13,21 @@ export class Item {
     if (this.name != 'Aged Brie' && this.name != 'Backstage passes to a TAFKAL80ETC concert') {
       if (this.quality > 0) {
         if (this.name != 'Sulfuras, Hand of Ragnaros') {
-          this.quality = this.quality - 1
+          this.quality -= 1
         }
       }
     } else {
       if (this.quality < 50) {
-        this.quality = this.quality + 1
+        this.quality += 1
         if (this.name == 'Backstage passes to a TAFKAL80ETC concert') {
           if (this.sellIn < 11) {
             if (this.quality < 50) {
-              this.quality = this.quality + 1
+              this.quality += 1
             }
           }
           if (this.sellIn < 6) {
             if (this.quality < 50) {
-              this.quality = this.quality + 1
+              this.quality += 1
             }
           }
         }
@@ -35,15 +35,15 @@ export class Item {
     }
 
     if (this.name != 'Sulfuras, Hand of Ragnaros') {
-      this.sellIn = this.sellIn - 1;
+      this.sellIn -= 1;
     }
-    
+
     if (this.sellIn < 0) {
       if (this.name != 'Aged Brie') {
         if (this.name != 'Backstage passes to a TAFKAL80ETC concert') {
           if (this.quality > 0) {
             if (this.name != 'Sulfuras, Hand of Ragnaros') {
-              this.quality = this.quality - 1
+              this.quality -= 1;
             }
           }
         } else {
@@ -51,7 +51,7 @@ export class Item {
         }
       } else {
         if (this.quality < 50) {
-          this.quality = this.quality + 1
+          this.quality += 1;
         }
       }
     }
